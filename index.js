ee6516b49dc9d73133701864eb788c980adb87c9
Yohannes90 <mekonnenyohannes90@gmail.com>
2026-04-04 21:01:23 +0300

    serving frontend/static files from the backend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', function getBody (req) {
   if (req.method !== 'POST' && req.method !== 'PUT') return ''
